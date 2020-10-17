d936acac6460443407f0342cbb326e8101be78a8
caitlinjee <caitlinjeespn@gmail.com>
2020-10-17 13:19:33 +0800

    Add serializers

--- lecturers/views.py
+++ lecturers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+from lecturers.models import Lecturer
+from lecturers.serializers import LecturerSerializer
+
+
+class LecturerList(generics.ListCreateAPIView):
+    queryset = Lecturer.objects.all()
+    serializer_class = LecturerSerializer
+
+
+class LecturerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lecturer.objects.all()
+    serializer_class = LecturerSerializer
